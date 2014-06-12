77ebce6bdc5906963410295f52fcfd52faff2f90
jain208 <abhi2000@gmail.com>
2014-06-12 17:02:33 +0100

    wip - added touch events

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -26,8 +26,8 @@ console.log('\'Allo \'Allo!');
         $log.html($log.html() + mousedownMessage);
     });
 */
-    AddMessageToLog(Modernizr.touch);
-
+    AddMessageToLog(Modernizr.touch ? "touch enabled!!" : "touch not enabled!!");
+    var isTouchEnabled = Modernizr.touch;
     var promo = '#app-promo';
     var $promo = $(promo);
     var $trigger = $('.flightapps');
@@ -36,42 +36,68 @@ console.log('\'Allo \'Allo!');
         var messageTag = '<div>' + message + '</div>';
         $log.html($log.html() + messageTag);
     }
-    $trigger.on('click', function () {
-        AddMessageToLog('Clicked!!');
-        if ($promo.position().top == -300) {
-            AddMessageToLog('Expanding!!');
-            $(this).addClass('appButtonSelected');
-            $promo.animate({
-                top: 0
-            });
-        }
 
-        if ($promo.position().top == 0) {
-            AddMessageToLog('Collapsing!!');
-            $(this).removeClass('appButtonSelected');
-            $promo.animate({
-                top: -300
-            });
-        }
-    });
+    function expandPanel() {
+        AddMessageToLog('Expanding!!');
+        $trigger.addClass('appButtonSelected');
+        $promo.animate({
+            top: 0
+        });
+    }
 
-    $(document).on('click', function (event) {
+    function collapsePanel() {
+        AddMessageToLog('Collapsing!!');
+        $trigger.removeClass('appButtonSelected');
+        $promo.animate({
+            top: -300
+        });
+    }
+    if (!Modernizr.touch) {
+        $trigger.on('click', function () {
+            AddMessageToLog('Clicked!!');
+            if ($promo.position().top == -300) {
+                expandPanel();
+            }
+
+            if ($promo.position().top == 0) {
+                collapsePanel();
+            }
+        });
+    }
+    if (Modernizr.touch) {
+        $trigger.on('touchstart', function() {
+            AddMessageToLog('touch start!!');
+        })
+
+        $trigger.on('touchmove', function(e) {
+            console.log(e);
+            expandPanel();
+            AddMessageToLog('touch move!!');
+        })
+
+        $trigger.on('touchend', function() {
+            AddMessageToLog('touch end!!');
+        })
+    }
+
+    var documentEventTrigger = Modernizr.touch ? "touchstart" : "click"
+    $(document).on(documentEventTrigger, function (event) {
         var target = event.target;
-        console.log(event.target);
-        console.log($trigger.get(0))
+        AddMessageToLog(documentEventTrigger);
         if (event.target != $trigger.get(0)) {
             if ($promo.position().top == 0) {
                 console.log($(event.target));
-                AddMessageToLog('Collapsing!!');
                 if (!$(event.target).parents('#app-promo').length) {
-                    $promo.animate({
-                        top: -300
-                    });
+                    collapsePanel();
                     $trigger.removeClass('appButtonSelected');
                 }
             }
         }
     });
+
+    $('#clearLog').on('click', function() {
+        $log.empty();
+    })
 
 
 })(window.jQuery, window.Modernizr)
